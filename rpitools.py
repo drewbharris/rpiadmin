b5f57c66e0611bebac0436f5f89092325d35c89e
Drew Harris <drew@drews-mac-pro.(none)>
2013-04-06 14:37:36 -0700

    Add some system routes

--- rpitools.py
+++ rpitools.py
@@ -1,11 +1,25 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/api/v1/system/shutdown')
+def shutdown():
+    os.system("sudo shutdown now")
+    return "ok"
+
+
+@app.route('/api/v1/system/reboot')
+def reboot():
+    os.system("sudo reboot")
+    return "ok"
+
 
 if __name__ == '__main__':
     app.run()
